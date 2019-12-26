af793f80e0c58a43d21b0d4d4ee84c87835304a8
denis290788 <denis290788@gmail.com>
2019-12-26 22:06:38 +0300

    commit 1

---
+++ src/test/java/DataGenerator.java
@@ -0,0 +1,32 @@
+import com.github.javafaker.Faker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class DataGenerator {
+    private DataGenerator() {}
+
+    public static class CardDeliveryRequest{
+        private CardDeliveryRequest() {}
+
+        public static CardDeliveryRequestByUserInfo generateByUserInfo() {
+            Faker faker = new Faker(new Locale("ru"));
+
+            Calendar instance = Calendar.getInstance();
+            Date dateNow = new Date();
+            SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd.MM.yyyy");
+            instance.setTime(dateNow);
+            instance.add(Calendar.DAY_OF_MONTH, 3);
+            String confirmedDate = formatForDateNow.format(instance.getTime());
+
+            return new CardDeliveryRequestByUserInfo(
+                    faker.name().fullName(),
+                    faker.phoneNumber().phoneNumber(),
+                    faker.address().city(),
+                    confirmedDate
+            );
+        }
+    }
+}
